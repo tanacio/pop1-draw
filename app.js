68a80c55585a6726a63c24e7b36cc1c716fb8d56
tanacio <hechima@tanacio.com>
2023-04-27 21:43:14 +0900

    add tag

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      "img-src": ["'self'", "https: data:"],
+      "img-src": ["'self'", "https: data:", "https://www.googletagmanager.com"],
+      "script-src": ["'self'", "https://www.googletagmanager.com"],
+      "connect-src": ["'self'", "https://www.googletagmanager.com"],
     }
   })
 )
